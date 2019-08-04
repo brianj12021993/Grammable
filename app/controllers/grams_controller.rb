967c609c470c5b3ca85cd798a980c25c5d953da3
Brian Johnson <brianjohnsona@yahoo.com>
2019-08-04 17:51:52 +0000

    amazon S3

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -54,8 +54,7 @@ class GramsController < ApplicationController
   	private
 
   	def gram_params
-    	params.require(:gram).permit(:message)
-    	params.require(:gram).permit(:picture)
+    	params.require(:gram).permit(:message, :picture)
   	end
 
   	def render_not_found(status=:not_found)
